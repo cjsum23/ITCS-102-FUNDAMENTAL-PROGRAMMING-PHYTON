86ea0f4a31fd85d5939fe6db32cfa48a4ca3bc01
Christian Jacob Sumagop <christiansumagop@gmail.com>
2025-08-11 15:45:05 +0800

    Codechallenge1.py

---
+++ codechallenge1.py
@@ -0,0 +1,3 @@
+a = input("WHAT'S YOUR NAME :")
+
+print("\t\t\t\t\t",  a, "\n\t\t\t\t\t * \n \t\t\t\t * \t\t * \n \t\t\t * \t\t\t\t * \n \t\t * \t\t\t HI\t\t\t * \n \t * \t\t\t\t", a, "\t\t\t\t * \n \t\t * \t\t\t\t\t\t *\n \t\t\t * \t\t\t\t *\n \t\t\t\t * \t\t *\n \t\t\t\t\t *\n")

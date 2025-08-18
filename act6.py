2a38714465034f9d2b5af0ef36d1d5fc8ec1a404
Christian Jacob Sumagop <christiansumagop@gmail.com>
2025-08-18 10:33:21 +0800

    ACT6

---
+++ act6.py
@@ -0,0 +1,16 @@
+n1 = eval(input("Enter Your First Numer ="))
+n2 = eval(input("Enter Your First Number ="))
+
+c = n1 + n2
+d = n1 - n2
+e = n1 * n2
+f = n1 / n2
+g = n1 ** n2
+h = n1 // n2
+
+print( n1,"+", n2,"=", c)
+print(n1,"-", n2,"=", d)
+print(n1,"*", n2,"=", e)
+print(n1,"/", n2,"=", f)
+print(n1,"**", n2,"=", g)
+print(n1,"//", n2,"=", h)

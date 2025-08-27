f46bcf07a47e2366d0f99fe0e4c678be6bb2f1e1
Christian Jacob Sumagop <christiansumagop@gmail.com>
2025-08-27 11:20:07 +0800

    CODE_CHALLENGE3
    LOGIN SYSTEM

---
+++ codechallenge3.py
@@ -0,0 +1,11 @@
+user = "CJSumagop"
+pas = "POGIeh"
+
+a = input("ENTER YOUR USERNAME --> ")
+b = input("ENTER YOUR PASWORD --> ")
+
+if a == user and b == pas:
+ print("Access Granted")   
+
+else:
+     print("Access denied")

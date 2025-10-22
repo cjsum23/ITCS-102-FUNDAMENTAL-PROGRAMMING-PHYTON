6d940c58547bc8c529b35d9359b85f5583ee036a
Christian Jacob Sumagop <christiansumagop@gmail.com>
2025-10-22 11:24:35 +0800

    Code_challenge13.py

---
+++ code_challenge13.py
@@ -0,0 +1,27 @@
+for i in range(1,5,1):
+    for c in range(9,i,-1):
+        print(" ", end=" ")
+    for d in range(1,i,1):
+        print("*", end=" ")
+    for x in range(1,i + 1,1):
+        print("*", end=" ")
+    print()
+for i in range(1,7,1):
+    for c in range(9,i,-1):
+        print(" ", end=" ")
+    for d in range(1,i,1):
+        print("*", end=" ")
+    for x in range(1,i + 1,1):
+        print("*", end=" ")
+    print()
+for i in range(1,9,1):
+    for c in range(9,i,-1):
+        print(" ", end=" ")
+    for d in range(1,i,1):
+        print("*", end=" ")
+    for x in range(1,i + 1,1):
+        print("*", end=" ")
+    print()
+for i in range(1,11,1):
+    for c in range(9,i-1):
+        print(c)

4905b6bf412d2a3e4201cd5d746c53364b2ed3fb
Christian Jacob Sumagop <christiansumagop@gmail.com>
2025-11-17 11:30:38 +0800

    Act23.py

---
+++ Act23.py
@@ -0,0 +1,26 @@
+months =['jan','feb','mar','april','may','june','jul']
+
+# months.append('Aug')
+# print(months)
+# months.pop()
+# print(months)
+# months.append('Aug')
+# print(months)
+# months.reverse()
+# print(months)
+
+# for m in months:
+#     print(f'{m} 2025')
+
+fullname = 'CJ Sumagop'
+
+# for i in fullname:
+#     print(i)
+
+# for c in fullname[::-1]:
+#     print(c)
+
+anthr = list(fullname)
+print(anthr)
+anthr.reverse()
+print(anthr)

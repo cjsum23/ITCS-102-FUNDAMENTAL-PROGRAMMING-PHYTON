566a0e173e81f71991de17832cf96b4b9a62ee17
Christian Jacob Sumagop <christiansumagop@gmail.com>
2025-11-21 11:34:34 +0800

    Update code_challenge16.py

--- code_challenge16.py
+++ code_challenge16.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 os.system('cls')
 
@@ -17,13 +18,13 @@ while True:
         print('----------------------------------')
 
         key = input('Enter Student Key --> ').lower()
-        fn = input('Enter Student First Name --> ').lower()
-        ln = input('Enter Student Last Name --> ').lower()
-        course = input('Enter Students Course --> ').lower()
-        email = input('Enter Student Email --> ').lower()
-        Status = input('Are You single(Yes / No) --> ').lower()
+        fn = input('Enter Student First Name --> ').upper()
+        ln = input('Enter Student Last Name --> ').upper()
+        course = input('Enter Students Course --> ').upper()
+        email = input('Enter Student Email --> ')
+        Status = input('Are You single(Yes / No) --> ').upper()
 
-        student_records = {key : [fn, ln, course, email, Status]}
+        student_records[key]= [fn, ln, course, email, Status]
         print('DATA SAVED')
 
         os.system('cls')
@@ -45,10 +46,65 @@ while True:
                 print('No record Found')
         continue
     elif choice == 'c':
-        delete = input('Enter Student key to delete --> ').lower
-        print('Record found')
+        os.system('cls')
+        SI = input(' Input ID search --> ').lower()
 
-        for j in student_records.keys():
-            if delete in student_records.key():
-                for i in student_records[delete]:
-                    del i 
+        if SI in student_records.keys():
+            print('\n \nRecord Found')
+            for i in student_records.keys():
+                print(f'-- {i}')
+            student_records.pop(SI)
+            print('Record Deleted')
+        else :
+                print('\n \nNo record found')
+
+                continue
+    elif choice == "d":
+        SI = input(' Input ID search --> ').lower()
+        for i in student_records.keys():
+            if SI in student_records.keys():
+                print('\n \nRecord Found')
+                print(f'-- {i}')
+            first_name = input('Enter New First Name --> ').upper()
+            last_name = input('Enter New Last Name --> ').upper()
+            course = input('Enter New Course --> ').upper()
+            email = input('Enter New email --> ')
+
+            student_records[SI][0] = first_name
+            student_records[SI][1] = last_name
+            student_records[SI][2] = course
+            student_records[SI][3] = email
+
+            print('Data Updated')
+
+
+        else :
+                print('\n \nNo record found')
+
+                continue
+    elif choice == 'e':
+        os.system('cls')
+        print('Exporting Data')
+
+        with open('student_records.json', 'w') as new_file:
+             json.dump(student_records, new_file)
+
+        print('Data exported to JSON')
+        continue
+
+    elif choice == 'f':
+        os.system('cls')
+        print('Importing Data')
+
+        with open('student_records.json', 'r') as new_file:
+            student_json = json.load(new_file)
+
+        student_records = student_json
+        print('Data exported to JSON')
+        continue
+
+    elif choice == 'g':
+        print('Program closing')
+        break
+
+            

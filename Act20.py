56d77a04131df7b7907608780843e00d189f4f6c
Christian Jacob Sumagop <christiansumagop@gmail.com>
2025-12-14 17:02:33 +0800

    Act20.py

---
+++ Act20.py
@@ -0,0 +1,7 @@
+def Act20():
+    for me in range(1,11):
+        for u in range(1,me,1):
+            print("*",end=" ")
+        for th in range(10,me,-1):
+            print("*",end=" ")
+        print()   

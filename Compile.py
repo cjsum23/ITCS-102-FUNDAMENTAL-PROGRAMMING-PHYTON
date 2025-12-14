a4cd6603a70945114cff952a9dae787ea138696d
Christian Jacob Sumagop <christiansumagop@gmail.com>
2025-12-14 16:53:55 +0800

    Menu_Compile.py

---
+++ Compile.py
@@ -0,0 +1,35 @@
+
+from Act1 import Act1
+from Act2 import Act2
+from Act3 import Act3
+from Act4 import Act4
+from Act5 import Act5
+from Act6 import Act6
+from Act7 import Act7
+from Act8 import Act8
+from Act9 import Act9
+from Act10 import Act10
+from Act11 import Act11
+from Act12 import Act12
+from Act13 import Act13
+from Act14 import Act14
+from Act15 import Act15
+from act16 import Act16
+from Act17 import Act17
+from Act18 import Act18
+from Act19 import Act19
+from Act20 import Act20
+from Act21 import Act21
+from Act22 import Act22
+from Act23 import Act23
+from Act24_1 import Act241
+from Act25_1 import Act251
+from Act26 import Act26
+from Act27 import Act27
+
+
+from codechallenge1 import *
+from code_challenge2 import *
+from CC3 import *
+from CC4 import *
+from code_challenge5 import *
